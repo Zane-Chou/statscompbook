98b934d4686fd88bcc125d857420839fda34583b
Yanfei Kang <yanfei.kang@outlook.com>
2020-03-17 12:11:54 +0800

    add newton optim

--- newtonraphson/newton-animation.R
+++ newtonraphson/newton-animation.R
@@ -7,20 +7,22 @@ diff.x0 <- attr(f_value, "gradient")[1]
 tol <- 1e-9
 maxiter <- 5
 count <- 0
-x0 <- 4
+x0 <- 6
 f_value <- f(x0)
 f.x0 <- f_value[1]
 diff.x0 <- attr(f_value, "gradient")[1] 
-saveGIF(
+animation::saveGIF(
     {curve(f, -6 , 6, col = 2)
         abline(h=0, col = 'gray')
         segments(x0, 0, x0, f.x0, lty = 2)
+        text(x0, -10, expression(x[0]))
         legend("bottomright", c('Function', 'Tangent'), col = c(2,3), lty = c(1, 2))
-        curve(f, -6 , 6)
+        curve(f, -6 , 6, col = 2)
         abline(h=0, col = 'gray')
         segments(x0, 0, x0, f.x0, lty = 2)
+        text(x0, -10, expression(x[0]))
+        legend("bottomright", c('Function', 'Tangent'), col = c(2,3), lty = c(1, 2))
         abline(a = f.x0 - x0*diff.x0, b = diff.x0, col = 3, lty = 2)
-        legend("bottomright", c('Function', 'Tangent'), col = c(2,3), lty = c(1,2))
         while (abs(f.x0) > tol && count < maxiter){
             xn <- x0-f.x0/diff.x0
             x0 <- xn
@@ -31,17 +33,18 @@ saveGIF(
             curve(f, -6, 6, col = 2)
             abline(h=0, col = 'gray')
             segments(x0, 0, x0, f.x0,  lty = 2)
+            text(x0, -10, bquote('x'[.(count)]))
             legend("bottomright", c('Function', 'Tangent'), col = c(2,3), lty = c(1,2))
             if (count < maxiter){
-            Sys.sleep(1)
-            curve(f, -6, 6, col = 2)
-            abline(h=0, col = 'gray')
-            segments(x0, 0, x0, f.x0,  lty = 2)
-            abline(a = f.x0 - x0*diff.x0, b = diff.x0, col = 3, lty = 2)
-            legend("bottomright", c('Function', 'Tangent'), col = c(2,3), lty = c(1,2))}
+                curve(f, -6, 6, col = 2)
+                abline(h=0, col = 'gray')
+                segments(x0, 0, x0, f.x0,  lty = 2)
+                text(x0, -10, bquote('x'[.(count)]))
+                legend("bottomright", c('Function', 'Tangent'), col = c(2,3), lty = c(1,2))
+                abline(a = f.x0 - x0*diff.x0, b = diff.x0, col = 3, lty = 2)}
         }}, movie.name = 'newton.gif',
     interval = 1, 
     ani.width = 600, 
-    ani.height = 400,
-    outdir = getwd())
+    ani.height = 600,
+    outdir = '~/Documents/Statistical-Computing-Chinese/newtonraphson/')
 
